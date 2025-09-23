6f842e8470f8c835cbf43fcf11a783f01810b2e5
Brandon Kapp <47344529+pm6guy10@users.noreply.github.com>
2025-09-23 12:10:47 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-  experimental: {
-    appDir: true,
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
