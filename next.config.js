c3bc7ab1c1683fd49a475232d127fc877eb13a63
Brandon Kapp <47344529+pm6guy10@users.noreply.github.com>
2025-09-23 11:51:01 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  output: 'standalone',
   experimental: {
     appDir: true,
-  },
-  env: {
-    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.VITE_STRIPE_PUBLIC_KEY,
   },
 }
 
