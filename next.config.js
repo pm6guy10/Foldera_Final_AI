91df819b6522e2b65f1de771e33b881700a7c941
Brandon Kapp <47344529+pm6guy10@users.noreply.github.com>
2025-09-25 01:42:25 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,13 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    appDir: true,
-  },
-  env: {
-    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.VITE_STRIPE_PUBLIC_KEY,
+  images: {
+    domains: ['storage.googleapis.com'],
   },
 }
 
-module.exports = nextConfig
+export default nextConfig
